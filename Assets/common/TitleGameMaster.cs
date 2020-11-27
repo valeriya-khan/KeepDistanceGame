e14bc360c4269095c079766a2d6cb7b89cf7857a
khanV <valeriya.khan@nu.edu.kz>
2020-11-27 12:32:13 +0900

    transitions from title scene

--- Assets/common/TitleGameMaster.cs
+++ Assets/common/TitleGameMaster.cs
@@ -9,5 +9,9 @@ public class TitleGameMaster : MonoBehaviour
     public void GoToGameScene(){
         SceneManager.LoadScene("gameScene");
     }
+    public void GoToLevelMenu(){
+        SceneManager.LoadScene("levelMenu");
+    }
+
 
 }
